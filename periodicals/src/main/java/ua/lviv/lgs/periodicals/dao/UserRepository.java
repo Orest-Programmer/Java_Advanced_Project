fb82334388abfd62e2b33d7c8f7e94aa6e901a24
DESKTOP-HOEE20R\HILL <orestmusicstudio@gmail.com>
2023-01-18 12:00:07 +0200

    Add image upload

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/dao/UserRepository.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/dao/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.lviv.lgs.periodicals.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	Optional<User> findByEmail(String email);
+	public Optional<User> findByEmail(String email);
 }
