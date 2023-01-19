defda9192f892433f22086c8e8cf28a22087d0ba
DESKTOP-HOEE20R\HILL <orestmusicstudio@gmail.com>
2023-01-19 14:49:02 +0200

    Add role base access

--- periodicals/src/main/java/ua/lviv/lgs/periodicals/service/UserService.java
+++ periodicals/src/main/java/ua/lviv/lgs/periodicals/service/UserService.java
@@ -1,5 +1,7 @@
 package ua.lviv.lgs.periodicals.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -9,23 +11,28 @@ import ua.lviv.lgs.periodicals.domain.User;
 import ua.lviv.lgs.periodicals.domain.UserRole;
 
 @Service
-public class UserService{
-    @Autowired
-    private UserRepository userRepository;
+public class UserService {
 
-    @Autowired
-    private PasswordEncoder bCryptPasswordEncoder;
+	private Logger logger = LoggerFactory.getLogger(UserService.class);
 
+	@Autowired
+	private UserRepository userRepository;
 
-    public void save(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPasswordConfirm()));
-        user.setRole(UserRole.ROLE_USER);
-        userRepository.save(user);
-    }
-    
-    public User findByEmail(String email) {
-    	return userRepository.findByEmail(email).get();
-    }
+	@Autowired
+	private PasswordEncoder bCryptPasswordEncoder;
+
+	public void save(User user) {
+		logger.info("Register new user {} : " + user);
+
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPasswordConfirm()));
+		user.setRole(UserRole.ROLE_USER);
+		userRepository.save(user);
+	}
+
+	public User findByEmail(String email) {
+		logger.info("Get user {} by email: " + email);
+		return userRepository.findByEmail(email).get();
+	}
 
 }
